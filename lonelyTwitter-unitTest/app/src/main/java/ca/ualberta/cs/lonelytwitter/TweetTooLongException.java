e5c863fc52f93a5804a9e388f73e2e9451bc1eb6
brianzbw <brian.zbw@gmail.com>
2017-03-01 16:07:47 -0700

    for lab7

--- lonelyTwitter-unitTest/app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++
@@ -1,7 +0,0 @@
-package ca.ualberta.cs.lonelytwitter;
-
-/**
- * Created by romansky on 1/14/16.
- */
-public class TweetTooLongException extends Exception {
-}
--- lonelyTwitter-w17Thursday/app/src/main/java/ca/ualberta/cs/lonelytwitter/TweetTooLongException.java
+++
@@ -1,8 +0,0 @@
-package ca.ualberta.cs.lonelytwitter;
-
-/**
- * Created by shida3 on 1/19/17.
- */
-
-public class TweetTooLongException extends Exception {
-}
